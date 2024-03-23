3685671e70acb3c502857c11bb26f2b07852ac06
na4ma4 <na4ma4@users.noreply.github.com>
2024-03-24 09:18:24 +1000

    work-in-progress

--- context.go
+++ context.go
@@ -2,91 +2,16 @@ package contextual
 
 import (
 	"context"
-	"time"
-
-	"github.com/na4ma4/go-contextual/health"
-	"github.com/na4ma4/go-zaptool"
-	"golang.org/x/sync/errgroup"
 )
 
-//nolint:containedctx // It's a context wrapper that includes errgroup.Group and context.CancelFunc.
-type CtxCancel struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-	errg   *errgroup.Group
-	health health.Health
-}
+type Context interface {
+	context.Context
 
-func NewCtxCancel(ctx context.Context, logmgr zaptool.LogManager) *CtxCancel {
-	ctx, cancel := context.WithCancel(ctx)
-	errg, ctx := errgroup.WithContext(ctx)
-	health := health.NewCore(logmgr.Named("Health"))
-
-	return &CtxCancel{
-		ctx:    ctx,
-		cancel: cancel,
-		errg:   errg,
-		health: health,
-	}
-}
-
-func (c *CtxCancel) Health() health.Health {
-	return c.health
-}
-
-// Cancel calls the context.CancelFunc.
-// A CancelFunc tells an operation to abandon its work.
-// A CancelFunc does not wait for the work to stop.
-// A CancelFunc may be called by multiple goroutines simultaneously.
-// After the first call, subsequent calls to a CancelFunc do nothing.
-func (c *CtxCancel) Cancel() {
-	c.cancel()
-}
-
-// Wait blocks until all function calls from the Go method have returned, then
-// returns the first non-nil error (if any) from them.
-//
-//nolint:wrapcheck // transparent method to call internal context.
-func (c *CtxCancel) Wait() error {
-	return c.errg.Wait()
-}
-
-// Go calls the given function in a new goroutine.
-//
-// The first call to return a non-nil error cancels the group; its error will be
-// returned by Wait.
-func (c *CtxCancel) Go(f func() error) {
-	c.errg.Go(f)
-}
-
-func (c *CtxCancel) Deadline() (time.Time, bool) {
-	return c.ctx.Deadline()
-}
-
-// Done returns a channel that's closed when work done on behalf of this
-// context should be canceled. Done may return nil if this context can
-// never be canceled. Successive calls to Done return the same value.
-// The close of the Done channel may happen asynchronously,
-// after the cancel function returns.
-func (c *CtxCancel) Done() <-chan struct{} {
-	return c.ctx.Done()
-}
-
-// Err returns the context error.
-// If Done is not yet closed, Err returns nil.
-// If Done is closed, Err returns a non-nil error explaining why:
-// Canceled if the context was canceled
-// or DeadlineExceeded if the context's deadline passed.
-// After Err returns a non-nil error, successive calls to Err return the same error.
-//
-//nolint:wrapcheck // transparent method to call internal context.
-func (c *CtxCancel) Err() error {
-	return c.ctx.Err()
-}
-
-// Value returns the value associated with this context for key, or nil
-// if no value is associated with key. Successive calls to Value with
-// the same key returns the same result.
-func (c *CtxCancel) Value(key any) any {
-	return c.ctx.Value(key)
+	Cancel()
+	CancelWithCause(err error)
+	CloneWithNewContext(ctx context.Context, cancel context.CancelCauseFunc) Context
+	Go(f func() error)
+	Wait() error
+	// Health() health.Health
+	ReplaceContext(cb func(context.Context) context.Context)
 }
--- func.go
+++ func.go
@@ -4,24 +4,108 @@ import (
 	"context"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
-func WithCancel(ctx *CtxCancel) (*CtxCancel, context.CancelFunc) {
-	rawCtx, cancel := context.WithCancel(ctx)
-	return &CtxCancel{
-		ctx:    rawCtx,
-		cancel: cancel,
-		errg:   ctx.errg,
-		health: ctx.health,
-	}, cancel
+// WithTimeout returns WithDeadline(parent, time.Now().Add(timeout)).
+//
+// Canceling this context releases resources associated with it, so code should
+// call cancel as soon as the operations running in this [Context] complete:
+//
+//	func slowOperationWithTimeout(ctx context.Context) (Result, error) {
+//		ctx, cancel := context.WithTimeout(ctx, 100*time.Millisecond)
+//		defer cancel()  // releases resources if slowOperation completes before timeout elapses
+//		return slowOperation(ctx)
+//	}
+func WithTimeout(parent context.Context, timeout time.Duration) (Context, context.CancelFunc) {
+	return WithDeadline(parent, time.Now().Add(timeout))
 }
 
-func WithSignalCancel(ctx *CtxCancel) (*CtxCancel, context.CancelFunc) {
+// WithTimeoutCause behaves like [WithTimeout] but also sets the cause of the
+// returned Context when the timeout expires. The returned [CancelFunc] does
+// not set the cause.
+func WithTimeoutCause(parent context.Context, timeout time.Duration, cause error) (Context, context.CancelFunc) {
+	return WithDeadlineCause(parent, time.Now().Add(timeout), cause)
+}
+
+// WithDeadline returns a copy of the parent context with the deadline adjusted
+// to be no later than d. If the parent's deadline is already earlier than d,
+// WithDeadline(parent, d) is semantically equivalent to parent. The returned
+// [Context.Done] channel is closed when the deadline expires, when the returned
+// cancel function is called, or when the parent context's Done channel is
+// closed, whichever happens first.
+//
+// Canceling this context releases resources associated with it, so code should
+// call cancel as soon as the operations running in this [Context] complete.
+func WithDeadline(parent context.Context, d time.Time) (Context, context.CancelFunc) {
+	return WithDeadlineCause(parent, d, nil)
+}
+
+// WithDeadlineCause behaves like [WithDeadline] but also sets the cause of the
+// returned Context when the deadline is exceeded. The returned [CancelFunc] does
+// not set the cause.
+func WithDeadlineCause(parent context.Context, d time.Time, cause error) (Context, context.CancelFunc) {
+	rootCtx, cancel := context.WithDeadlineCause(parent, d, cause)
+	var outCtx Context
+	if parentIsCtxl, ok := parent.(Context); ok {
+		outCtx = parentIsCtxl.CloneWithNewContext(rootCtx, CancelCauseWrap(cancel))
+	} else {
+		outCtx = NewCancellable(rootCtx)
+	}
+
+	return outCtx, outCtx.Cancel
+}
+
+// WithCancel returns a copy of parent with a new Done channel. The returned
+// context's Done channel is closed when the returned cancel function is called
+// or when the parent context's Done channel is closed, whichever happens first.
+//
+// Canceling this context releases resources associated with it, so code should
+// call cancel as soon as the operations running in this Context complete.
+func WithCancel(ctx Context) (Context, context.CancelFunc) {
+	rootCtx := ctx.CloneWithNewContext(context.WithCancelCause(ctx))
+	return rootCtx, rootCtx.Cancel
+}
+
+// WithCancelCause behaves like [WithCancel] but returns a [CancelCauseFunc] instead of a [CancelFunc].
+// Calling cancel with a non-nil error (the "cause") records that error in ctx;
+// it can then be retrieved using Cause(ctx).
+// Calling cancel with nil sets the cause to Canceled.
+//
+// Example use:
+//
+//	ctx, cancel := context.WithCancelCause(parent)
+//	cancel(myError)
+//	ctx.Err() // returns context.Canceled
+//	context.Cause(ctx) // returns myError
+func WithCancelCause(ctx Context) (Context, context.CancelCauseFunc) {
+	rootCtx := ctx.CloneWithNewContext(context.WithCancelCause(ctx))
+	return rootCtx, rootCtx.CancelWithCause
+}
+
+// NotifyContext returns a copy of the parent context that is marked done
+// (its Done channel is closed) when one of the listed signals arrives,
+// when the returned stop function is called, or when the parent context's
+// Done channel is closed, whichever happens first.
+//
+// The stop function unregisters the signal behavior, which, like signal.Reset,
+// may restore the default behavior for a given signal. For example, the default
+// behavior of a Go program receiving os.Interrupt is to exit. Calling
+// NotifyContext(parent, os.Interrupt) will change the behavior to cancel
+// the returned context. Future interrupts received will not trigger the default
+// (exit) behavior until the returned stop function is called.
+//
+// The stop function releases resources associated with it, so code should
+// call stop as soon as the operations running in this Context complete and
+// signals no longer need to be diverted to the context.
+func WithSignalCancel(ctx Context) (Context, context.CancelFunc) {
 	rawCtx, cancel := signal.NotifyContext(ctx, syscall.SIGTERM, syscall.SIGINT)
-	return &CtxCancel{
-		ctx:    rawCtx,
-		cancel: cancel,
-		errg:   ctx.errg,
-		health: ctx.health,
-	}, cancel
+	rootCtx := ctx.CloneWithNewContext(rawCtx, CancelCauseWrap(cancel))
+	return rootCtx, rootCtx.Cancel
+}
+
+func CancelCauseWrap(cancel context.CancelFunc) context.CancelCauseFunc {
+	return func(_ error) {
+		cancel()
+	}
 }
